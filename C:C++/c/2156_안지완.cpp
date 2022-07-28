fd1612c96f38bab44029209d5f1645f1f019f7a0
Jason Ahn <synoti21@naver.com>
2022-07-29 00:51:29 +0900

    Update 2156_안지완.cpp

--- C:C++/c/2156_안지완.cpp
+++ C:C++/c/2156_안지완.cpp
@@ -4,32 +4,22 @@ using namespace std;
 
 int arr[10000];
 int dp[10000];
-
 int max(int a, int b){
     return a > b ? a : b;
 }
-
 int sol(int num){
     int maxD = 0;
-
     if(num == 0){ return 0;}
     if(num == 1){ return arr[0];}
     maxD = max(arr[num-1] + dp[num-3], dp[num-2]);
     return maxD;
 }
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int N, ans = 0;
     cin >> N;
-
-
     for(int i =0 ; i < N ; i++){
         cin >> arr[i];
     }
-
     for(int i =0 ; i < N ;i++){
         dp[i] = sol(i) + arr[i];
         if(i>0){

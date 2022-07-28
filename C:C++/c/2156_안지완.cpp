dff514f5b576d5eb0bc70ac1e4270febc463608d
Jason Ahn <synoti21@naver.com>
2022-07-29 00:50:38 +0900

    Update 2156_안지완.cpp

--- C:C++/c/2156_안지완.cpp
+++ C:C++/c/2156_안지완.cpp
@@ -14,31 +14,7 @@ int sol(int num){
 
     if(num == 0){ return 0;}
     if(num == 1){ return arr[0];}
-
-    /*for(int i = 3 ; i < num ; i++){
-        for(int j=i; num-j > 0; j++){
-            if(num-j-2 > 0){
-                maxD = max(maxD, arr[num-j]+dp[num-j-2]);
-            }else{
-                maxD = max(maxD, arr[num-j])
-            }
-        }*/
-
-    /*for(int i = 0 ; i < num ; i++){
-        if(num-i-2 > 0){
-            maxD = max(maxD, arr[num-i-1]+dp[num-i-2-1]);
-            //cout << "num : " << num <<  " i : " << i << " maxD : " << maxD << endl;
-        }else{
-            maxD = max(maxD, dp[num-i-2]);
-            //cout << "num : " << num << " i : " << i << " maxD : " << maxD << endl;
-        }
-    }*/
     maxD = max(arr[num-1] + dp[num-3], dp[num-2]);
-
-        //cout << "num : " << num <<  " i : " << i << " maxD : " << maxD << endl;
-
-        //cout << "num : " << num << " i : " << i << " maxD : " << maxD << endl;
-
     return maxD;
 }
 int main(void){
@@ -57,13 +33,9 @@ int main(void){
     for(int i =0 ; i < N ;i++){
         dp[i] = sol(i) + arr[i];
         if(i>0){
-            dp[i] = max(dp[i], dp[i-1]); //연속 2회 안 마실경우
+            dp[i] = max(dp[i], dp[i-1]); 
         }
         ans = dp[i];
-        //cout << " ans : " << ans << endl;
     }
     cout << ans;
 }
-
-
-
